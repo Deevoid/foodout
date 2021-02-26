631b28ac37ece73d52b4d2a1a7581abcaaf7f246
Deevoid <dev.4.pandey@gmail.com>
2021-02-26 23:34:51 +0530

    tailwind not working still, initial commit

---
+++ components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Style from './styles.scss'
+
+export default function Login() {
+  return (
+    <div className='div'>
+      Login component
+      <style jsx>{Style}</style>
+    </div>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import style from "../styles/globals.scss?type=global";
+import "../styles/style.css";
+// import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Component {...pageProps} />
+      <style jsx>{style}</style>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
